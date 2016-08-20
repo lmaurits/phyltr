a0a311a4e43aaafbf12e70ba6ec692e8f0045ea1
Luke Maurits <luke@maurits.id.au>
2016-08-20 16:18:26 +1200

    DendroPy conversion for nexus.

--- phyltr/commands/nexus.py
+++ phyltr/commands/nexus.py
@@ -13,7 +13,7 @@ OPTIONS:
 
 import fileinput
 
-import ete2
+import dendropy
 
 import phyltr.utils.phyoptparse as optparse
 
@@ -23,45 +23,12 @@ def run():
     parser = optparse.OptionParser(__doc__)
     options, files = parser.parse_args()
 
+    treelist = dendropy.TreeList()
     # Read trees
     first = True
     for count, line in enumerate(fileinput.input(files),1):
-        t = ete2.Tree(line)
-        leaves = t.get_leaves()
-        # If first tree, get names
-        if first:
-            names = sorted([l.name for l in leaves])
-            print_middle(names)
-            first = False
-        for l in leaves:
-            if l.name in names:
-                l.name = str(names.index(l.name)+1)
+        t = dendropy.Tree.get_from_string(line,schema="newick",rooting="default-rooted")
+        treelist.append(t)
 
-        # Print tree line
-        print "TREE tree_%d = %s" % (count, t.write())
-    
-    print_footer()
-
-    # Done
+    print treelist.as_string(schema="nexus", suppress_rooting=True, suppress_annotations=False, translate_tree_taxa=True)
     return 0
-
-def print_header():
-    print "#NEXUS\n"
-
-def print_middle(names):
-    print "Begin taxa;"
-    print "\tDimensions ntax=%d;" % len(names)
-    print "\t\tTaxlabels"
-    for name in names:
-        print "\t\t\t%s" % name 
-    print "\t\t\t;"
-    print "End;"
-
-    print "Begin trees;"
-    print "\tTranslate"
-    for count, name in enumerate(names,1):
-        print "\t\t%d %s," % (count, name)
-    print "\t\t;"
-
-def print_footer():
-    print "End;"
