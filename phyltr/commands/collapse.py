300f0731b261553ac4c52a6a1927fd557faec9ae
Luke Maurits <luke@maurits.id.au>
2016-09-28 10:40:45 +1300

    Added support for collapsing by attribute, and include annotations on root node.

--- phyltr/commands/collapse.py
+++ phyltr/commands/collapse.py
@@ -44,47 +44,70 @@ def run():
 
     # Parse options
     parser = optparse.OptionParser(__doc__)
+    parser.add_option('-a', '--attribute', dest="attribute", default=None)
     parser.add_option('-t', '--translate',
-                help='Specifies the translation file.')
+                help='Specifies the translation file.',default=None)
     options, files = parser.parse_args()
 
     # Read translation file
-    try:
-        trans = read_clade_file(options.translate)
-    except IOError:
-        return 1
+    if options.translate:
+        try:
+            trans = read_clade_file(options.translate)
+        except IOError:
+            return 1
 
     # Read trees
     for line in fileinput.input(files):
         t = ete2.Tree(line)
-        cache = t.get_cached_content()
-        tree_leaves = cache[t]
-        for clade, name in trans:
-            # Get a list of leaves in this tree
-            clade_leaves = [l for l in tree_leaves if l.name in clade]
-            if not clade_leaves:
-                continue
-            # Check monophyly
-            if len(clade_leaves) == 1:
-                mrca = clade_leaves[0]  # .get_common_ancestor works oddly for singletons
-            else:
-                mrca = t.get_common_ancestor(clade_leaves)
-            mrca_leaves = cache[mrca]
-            if set(mrca_leaves) == set(clade_leaves):
-                # Clade is monophyletic, so rename and prune
-                # But don't mess up distances
-                mrca.name = name
-                leaf, dist = mrca.get_farthest_leaf()
-                mrca.dist += dist
-                for child in mrca.get_children():
-                    child.detach()
-            else:
-                # Clade is not monophyletic.  We can't collapse it.
-                sys.stderr.write("Monophyly failure for clade: %s\n" % name)
-                return 1
+        if options.translate:
+            collapse_from_file(t, trans)
+        else:
+            collapse_by_attribute(t, options.attribute)
 
         # Output
-        print t.write()
+        print t.write(features=[],format_root_node=True)
 
     # Done
     return 0
+
+def collapse_by_attribute(t, attribute):
+    values = set([getattr(n,attribute) for n in t.traverse() if hasattr(n,attribute)])
+    if not t.check_monophyly(values, attribute, ignore_missing=True):
+        sys.stderr.write("Monophyly failure for attribute: %s" % attribute)
+        return 1
+    for v in values:
+        mrca = list(t.get_monophyletic([v], attribute))[0]
+        mrca.name = v
+        mrca.add_feature(attribute, v)
+        leaf, dist = mrca.get_farthest_leaf()
+        mrca.dist += dist
+        for child in mrca.get_children():
+            child.detach()
+    
+def collapse_from_file(t, trans):
+    cache = t.get_cached_content()
+    tree_leaves = cache[t]
+    for clade, name in trans:
+        # Get a list of leaves in this tree
+        clade_leaves = [l for l in tree_leaves if l.name in clade]
+        if not clade_leaves:
+            continue
+        # Check monophyly
+        if len(clade_leaves) == 1:
+            mrca = clade_leaves[0]  # .get_common_ancestor works oddly for singletons
+        else:
+            mrca = t.get_common_ancestor(clade_leaves)
+        mrca_leaves = cache[mrca]
+        if set(mrca_leaves) == set(clade_leaves):
+            # Clade is monophyletic, so rename and prune
+            # But don't mess up distances
+            mrca.name = name
+            leaf, dist = mrca.get_farthest_leaf()
+            mrca.dist += dist
+            for child in mrca.get_children():
+                child.detach()
+        else:
+            # Clade is not monophyletic.  We can't collapse it.
+            sys.stderr.write("Monophyly failure for clade: %s\n" % name)
+            sys.stderr.write("Interlopers: %s\n" % ",".join([n.name for n in set(mrca_leaves) - set(clade_leaves)]))
+            return 1
