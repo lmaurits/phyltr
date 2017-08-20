39e3727e8b87c01ae73d87b906caeb7c3bf78f3a
Luke Maurits <luke@maurits.id.au>
2017-08-20 12:08:54 +0300

    Basic dedupe tests.

---
+++ tests/dedupe_tests.py
@@ -0,0 +1,18 @@
+import fileinput
+
+from phyltr.commands.generic import NewickParser
+from phyltr.commands.dedupe import Dedupe
+
+def test_dedupe():
+    lines = fileinput.input("tests/treefiles/duplicate_taxa.trees")
+    trees = list(NewickParser().consume(lines))
+    for t in trees:
+        orig_leaves = t.get_leaf_names()
+        assert len(orig_leaves) == 6
+        assert orig_leaves.count("A") == 2
+        assert all((orig_leaves.count(x) == 1  for x in ("B", "C", "E", "F")))
+    deduped = Dedupe().consume(trees)
+    for t in deduped:
+        leaves = t.get_leaf_names()
+        assert len(leaves) == 5
+        assert all((leaves.count(x) == 1  for x in ("A", "B", "C", "E", "F")))
