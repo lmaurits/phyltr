a6deac81c2c256b9f805fb99003c964cd6a07873
Luke Maurits <luke@maurits.id.au>
2017-08-20 17:29:38 +0300

    Basic test of annotation command.

---
+++ tests/annotation_tests.py
@@ -0,0 +1,15 @@
+import fileinput
+
+from phyltr.commands.generic import NewickParser
+from phyltr.commands.annotate import Annotate
+
+def test_annotate():
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = NewickParser().consume(lines)
+    annotated = Annotate("tests/argfiles/annotation.csv", "taxon").consume(trees)
+    for t in annotated:
+        t.write(features=[])
+        for l in t.get_leaves():
+            assert hasattr(l, "f1")
+            assert hasattr(l, "f2")
+            assert hasattr(l, "f3")
