3998c59b1071281db5a390094dc8a3c053a45d1c
Luke Maurits <luke@maurits.id.au>
2017-08-22 21:33:00 +0300

    Basic test of consensus command.

---
+++ tests/consensus_tests.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+import fileinput
+
+from phyltr.commands.generic import NewickParser
+from phyltr.commands.consensus import Consensus
+
+def test_consensus():
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = NewickParser().consume(lines)
+    consensus = list(Consensus().consume(trees))
+    assert len(consensus) == 1
