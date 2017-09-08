624623ec70bfd99813baa151ffe04e9183d60c8e
Luke Maurits <luke@maurits.id.au>
2017-09-08 20:27:00 +0300

    More thorough testing of prune command.

--- tests/prune_tests.py
+++ tests/prune_tests.py
@@ -2,6 +2,7 @@ import fileinput
 
 from phyltr.commands.generic import NewickParser
 from phyltr.commands.prune import Prune
+from phyltr.commands.annotate import Annotate
 
 def test_prune():
     lines = fileinput.input("tests/treefiles/basic.trees")
@@ -21,3 +22,20 @@ def test_inverse_prune():
         assert all((x in leaves for x in ("A", "B")))
         assert not any((x in leaves for x in ("C", "D", "E", "F")))
 
+def test_file_prune():
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = NewickParser().consume(lines)
+    pruned = Prune(filename="tests/argfiles/taxa_abc.txt").consume(trees)
+    for t in pruned:
+        leaves = t.get_leaf_names()
+        assert not any((x in leaves for x in ("A", "B", "C")))
+
+def test_annotation_prune():
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = NewickParser().consume(lines)
+    annotated = Annotate(filename="tests/argfiles/annotation.csv", key="taxon").consume(trees)
+    pruned = Prune(attribute="f1", value="0").consume(annotated)
+    for t in pruned:
+        print(t.write())
+        leaves = t.get_leaf_names()
+        assert not any((x in leaves for x in ("A", "B", "C")))
