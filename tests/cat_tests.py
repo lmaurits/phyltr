57c4b6e95d5a1870eb600c3c5384197e33e5b3a1
Luke Maurits <luke@maurits.id.au>
2017-09-08 15:54:38 +0300

    Test parsing a NEXUS file produced by BEAST using ComplexNewickParser.

--- tests/cat_tests.py
+++ tests/cat_tests.py
@@ -1,10 +1,17 @@
 import fileinput
 
-from phyltr.commands.generic import NewickParser
+from phyltr.commands.generic import NewickParser, ComplexNewickParser
 from phyltr.commands.cat import Cat
 
-def test_cat():
+def test_basic_cat():
     lines = fileinput.input("tests/treefiles/basic.trees")
     trees = NewickParser().consume(lines)
     subsampled = Cat(subsample=2).consume(trees)
     assert sum((1 for t in subsampled)) == 3
+
+def test_beast_nexus_output_cat():
+    lines = fileinput.input("tests/treefiles/beast_output.nex")
+    trees = ComplexNewickParser().consume(lines)
+    trees = Cat().consume(trees)
+    for t in trees:
+        assert len(t.get_leaves()) == 26
