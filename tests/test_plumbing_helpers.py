926d66371d2d3ee491f0bbc4c6cb852590b27da9
Luke Maurits <luke@maurits.id.au>
2017-09-12 16:43:15 +0300

    Test that the plumbing helpers run.

---
+++ tests/test_plumbing_helpers.py
@@ -0,0 +1,19 @@
+import phyltr.plumbing.helpers as helpers
+from phyltr.commands.cat import Cat
+from phyltr.commands.height import Height
+from phyltr.commands.taxa import Taxa
+
+def test_plumb_stdin():
+    helpers.plumb_stdin(Cat(), "tests/treefiles/basic.trees")
+
+def test_complex_plumb():
+    helpers.complex_plumb(Cat(), "tests/treefiles/beast_output.nex")
+
+def test_plumb_strings():
+    helpers.plumb_strings(Height(), "tests/treefiles/basic.trees")
+
+def test_plumb_list():
+    helpers.plumb_strings(Taxa(), "tests/treefiles/basic.trees")
+
+def test_plumb_null():
+    helpers.plumb_null(Cat(), "tests/treefiles/basic.trees")
