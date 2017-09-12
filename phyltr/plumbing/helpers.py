d5ae7a1265fb7dd9c6209a16432980e463a1338c
Luke Maurits <luke@maurits.id.au>
2017-09-12 16:42:58 +0300

    Always close fileinputs after they've been completely consumed.

--- phyltr/plumbing/helpers.py
+++ phyltr/plumbing/helpers.py
@@ -27,24 +27,28 @@ def plumb_stdin(command, files="-"):
     trees_from_stdin = NewickParser().consume(source)
     output_trees = command.consume(trees_from_stdin)
     NewickFormatter(sys.stdout).consume(output_trees)
+    source.close()
 
 def complex_plumb(command, files="-"):
     source = fileinput.input(files)
     trees_from_stdin = ComplexNewickParser().consume(source)
     output_trees = command.consume(trees_from_stdin)
     NewickFormatter(sys.stdout).consume(output_trees)
+    source.close()
 
 def plumb_strings(command, files="-"):
     source = fileinput.input(files)
     trees_from_stdin = NewickParser().consume(source)
     output_trees = command.consume(trees_from_stdin)
     StringFormatter(sys.stdout).consume(output_trees)
+    source.close()
 
 def plumb_list(command, files="-"):
     source = fileinput.input(files)
     trees_from_stdin = NewickParser().consume(source)
     output_lists = command.consume(trees_from_stdin)
     ListPerLineFormatter(sys.stdout).consume(output_lists)
+    source.close()
 
 def plumb_null(command, files="-"):
     source = fileinput.input(files)
@@ -53,4 +57,5 @@ def plumb_null(command, files="-"):
     # Silently pull trees through the pipeline
     for t in output_trees:
         pass
+    source.close()
 
