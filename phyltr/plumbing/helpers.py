9ebae2036e49590116702226b428089e9c769817
Luke Maurits <luke@maurits.id.au>
2017-09-12 16:14:48 +0300

    Initial implementation of programmatic pipeline construction.

--- phyltr/plumbing/helpers.py
+++ phyltr/plumbing/helpers.py
@@ -1,8 +1,26 @@
 import fileinput
+import importlib
+import shlex
 import sys
 
 from phyltr.plumbing.sources import NewickParser, ComplexNewickParser
 from phyltr.plumbing.sinks import NewickFormatter, StringFormatter, ListPerLineFormatter
+
+def build_pipeline(string, source):
+    components = string.split("|")
+    for n, args in enumerate(components):
+        args = shlex.split(args)
+        command = args[0]
+        command_module = importlib.import_module("phyltr.commands."+command)
+        print("Building this: ", args)
+        command_obj, files = command_module.init_from_args(args[1:])
+        if hasattr(command_obj, "taxa"):
+            print(command_obj.taxa)
+        if n==0:
+            generator = command_obj.consume(source)
+        else:
+            generator = command_obj.consume(generator)
+    return generator
 
 def plumb_stdin(command, files="-"):
     source = fileinput.input(files)
---
+++ tests/test_pipeline.py
@@ -0,0 +1,16 @@
+import fileinput
+
+from phyltr.plumbing.sources import NewickParser
+from phyltr.plumbing.helpers import build_pipeline
+
+def test_pipeline():
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = NewickParser().consume(lines)
+    output = build_pipeline("rename -f tests/argfiles/rename.txt | prune X,B", source=trees)
+    for t in output:
+        print(t.write())
+        leaves = t.get_leaf_names()
+        assert "A" not in leaves
+        assert "X" not in leaves
+        assert "B" not in leaves
+        assert all((x in leaves for x in ("C", "D", "E", "F")))
