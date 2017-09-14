867a9707ef65a253678a78f6ccdb31b33da1c14e
Luke Maurits <luke@maurits.id.au>
2017-09-14 14:17:40 +0300

    Add --label option to pretty, for consistency plot.

--- phyltr/commands/pretty.py
+++ phyltr/commands/pretty.py
@@ -8,6 +8,9 @@ OPTIONS:
     -c, --compress
         Compress highly supported clades to a single node
 
+    -l, --label
+        Specify the name of an attribute with which to label leaves
+
     files
         A whitespace-separated list of filenames to read treestreams from.
         Use a filename of "-" to read from stdin.  If no filenames are
@@ -20,13 +23,18 @@ from phyltr.plumbing.helpers import plumb_strings
 
 class Pretty(PhyltrCommand):
 
-    def __init__(self, compress=False):
+    def __init__(self, label="name", compress=False):
+        self.label = label
         self.compress = compress
 
     def process_tree(self, t):
-        # Add support to interior nodes
+        # Change node names to get the desired appearance
         for node in t.traverse():
-            if not node.is_leaf():
+            # Replace leaf node names with requested attribute
+            if node.is_leaf() and hasattr(node, self.label):
+                node.name = getattr(node, self.label)
+            # Add support to interior nodes
+            else:
                 node.name = "%.2f" % node.support
 
         # Collapse high probability clades
@@ -42,15 +50,17 @@ class Pretty(PhyltrCommand):
                     node.name = "(%.2f) %s" % (node.support, "+".join(sorted([l.name for l in node.get_leaves()])))
                     for child in node.get_children():
                         child.detach()
+
         return t.get_ascii()
 
 def init_from_args(*args):
     # Parse options
     parser = optparse.OptionParser(__doc__)
     parser.add_option('-c', '--compress', action="store_true", dest="compress", default=False)
+    parser.add_option('-l', '--label', default="name")
     options, files = parser.parse_args(*args)
     
-    pretty = Pretty(compress=options.compress)
+    pretty = Pretty(label=options.label, compress=options.compress)
     return pretty, files
 
 def run():
