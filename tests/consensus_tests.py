d0a10ba80173f6bfa036c35ec361595f007d8822
Luke Maurits <luke@maurits.id.au>
2017-09-14 16:46:45 +0300

    Test summarisation of attribute values on consensus trees.

--- tests/consensus_tests.py
+++ tests/consensus_tests.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 import fileinput
 
-from phyltr.plumbing.sources import NewickParser
+from phyltr.plumbing.sources import ComplexNewickParser, NewickParser
 from phyltr.commands.consensus import Consensus
 
 def test_consensus():
@@ -10,3 +10,11 @@ def test_consensus():
     trees = NewickParser().consume(lines)
     consensus = list(Consensus().consume(trees))
     assert len(consensus) == 1
+
+def test_annotation_consensus():
+    lines = fileinput.input("tests/treefiles/beast_output_rate_annotations.nex")
+    trees = ComplexNewickParser().consume(lines)
+    consensus = list(Consensus().consume(trees))[0]
+    for n in consensus.traverse():
+        for attr in ("rate_mean", "rate_median", "rate_HPD"):
+            assert hasattr(n, attr)
