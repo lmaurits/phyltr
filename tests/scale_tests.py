6c827b9733b57b4b53e5169b03c69d1aee68a9f7
Luke Maurits <luke@maurits.id.au>
2017-09-14 15:18:12 +0300

    More thorough testing of scale command.

--- tests/scale_tests.py
+++ tests/scale_tests.py
@@ -1,6 +1,9 @@
 import fileinput
 
 from phyltr.plumbing.sources import NewickParser
+from phyltr.plumbing.helpers import build_pipeline
+from phyltr.commands.height import Height
+from phyltr.commands.length import Length
 from phyltr.commands.scale import Scale
 
 def test_scale():
@@ -13,3 +16,26 @@ def test_scale():
     for old, new in zip(old_heights, new_heights):
         assert new == old * scale_factor
 
+def test_identity():
+    """Make sure scaling with a factor of 1.0 changes nothing."""
+
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = list(NewickParser().consume(lines))
+    unscaled_trees = Scale(1.0).consume(trees)
+    for t1, t2 in zip(trees, unscaled_trees):
+        assert t1.write() == t2.write()
+
+def test_roundtrip():
+    """Make sure scaling by x and then 1/x changes nothing."""
+    lines = fileinput.input("tests/treefiles/basic.trees")
+    trees = list(NewickParser().consume(lines))
+
+    heights = Height().consume(trees)
+    scaled_heights = build_pipeline("scale -s 2.0 | scale -s 0.5 | height", trees)
+    for x, y in zip(heights, scaled_heights):
+        assert x == y
+
+    lengths = Length().consume(trees)
+    scaled_lengths = build_pipeline("scale -s 2.0 | scale -s 0.5 | length", trees)
+    for x, y in zip(lengths, scaled_lengths):
+        assert x == y
