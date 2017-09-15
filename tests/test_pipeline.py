ec75b13da64d4a1321a0c872de870889224d0284
Luke Maurits <luke@maurits.id.au>
2017-09-15 11:12:07 +0300

    Throw dupe into the big long silly test pipeline.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,10 +8,11 @@ def test_pipeline():
 
     lines = fileinput.input("tests/treefiles/basic.trees")
     trees = NewickParser().consume(lines)
-    output = build_pipeline("cat -s 2 | rename -f tests/argfiles/rename.txt | prune X,B | support --sort | uniq | stat", source=trees)
+    output = build_pipeline("cat -s 2 | rename -f tests/argfiles/rename.txt | prune X,B | dedupe | uniq | support --sort | stat", source=trees)
     for t in output:
         print(t.write())
         leaves = t.get_leaf_names()
+        assert all((leaves.count(x) == 1  for x in leaves))
         assert "A" not in leaves
         assert "X" not in leaves
         assert "B" not in leaves
