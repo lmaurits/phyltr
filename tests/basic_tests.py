08b928c7a5bb1b0a68b39a1dce1d59b32a5470ad
Luke Maurits <luke@maurits.id.au>
2017-09-15 11:11:27 +0300

    Test that ComplexNewickParser can be fed a file of non-tree content and correctly yield zero trees.

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -16,3 +16,9 @@ def test_complex_parser_on_non_newick():
     lines = fileinput.input("tests/treefiles/internal_names.trees")
     trees = ComplexNewickParser().consume(lines)
     assert sum((1 for t in trees)) == 6
+
+def test_complex_parser_on_non_tree():
+    lines = fileinput.input("tests/treefiles/not_trees.trees")
+    trees = ComplexNewickParser().consume(lines)
+    print(list(trees))
+    assert sum((1 for t in trees)) == 0
