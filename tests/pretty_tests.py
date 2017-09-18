4908fb5d0b8a7396870323071b949d49fb6d6469
Luke Maurits <luke@maurits.id.au>
2017-09-18 10:15:46 +0300

    Get better coverage of pretty --compress option by actually annotating treestreams with support before feeding them to pretty!

--- tests/pretty_tests.py
+++ tests/pretty_tests.py
@@ -2,6 +2,7 @@ import fileinput
 import shlex
 
 from phyltr.plumbing.sources import NewickParser
+from phyltr.plumbing.helpers import build_pipeline
 from phyltr.commands.pretty import Pretty, init_from_args
 
 def test_init_from_args():
@@ -21,14 +22,14 @@ def test_pretty():
     for x in Pretty().consume(trees):
         assert type(x) == str
 
-def test_pretty_compressed():
-    lines = fileinput.input("tests/treefiles/basic.trees")
-    trees = NewickParser().consume(lines)
-    for x in Pretty(compress=True).consume(trees):
-        assert type(x) == str
-
 def test_pretty_compressed_low_signal():
     lines = fileinput.input("tests/treefiles/low_signal.trees")
     trees = NewickParser().consume(lines)
-    for x in Pretty(compress=True).consume(trees):
+    for x in build_pipeline("support | pretty --compress", trees):
+        assert type(x) == str
+
+def test_pretty_compressed_mid_signal():
+    lines = fileinput.input("tests/treefiles/mid_signal.trees")
+    trees = NewickParser().consume(lines)
+    for x in build_pipeline("support | pretty --compress", trees):
         assert type(x) == str
